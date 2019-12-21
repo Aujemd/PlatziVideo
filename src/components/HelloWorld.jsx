f456bf50729791bf48d92cdfcac6e312b3288599
Johan Mora <joenmodu99@gmail.com>
2019-12-21 17:52:28 -0400

    Agregando compatibilidad con babel

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+        </div>
+    )
+}
+
+export default HelloWorld;//Creando componente stateless
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'//Importando react
+import ReactDOM from 'react-dom'//Importando react dom
+import HelloWorld from './components/HelloWorld'//Importando el componente creado por nosotros
+
+ReactDOM.render(<HelloWorld/>, document.getElementById(app));//Rendereando el componente en el div app del public html
