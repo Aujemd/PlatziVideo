6ec9c5d0b31c46ad3a0ac85c4c6fec094a9f84a1
Johan Mora <joenmodu99@gmail.com>
2019-12-22 19:41:30 -0400

    Reporte de errores y cambios en real time

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'//Importando react
 import ReactDOM from 'react-dom'//Importando react dom
 import HelloWorld from './components/HelloWorld'//Importando el componente creado por nosotros
 
-ReactDOM.render(<HelloWorld/>, document.getElementById(app));//Rendereando el componente en el div app del public html
+ReactDOM.render(<HelloWorld/>, document.getElementById("app"));//Rendereando el componente en el div app del public html
