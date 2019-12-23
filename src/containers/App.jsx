2604b08d7b03bdf705afb7c022de1694b858ba30
Johan Mora <joenmodu99@gmail.com>
2019-12-23 11:19:08 -0400

    Creacion estructura y estilo de componente Search

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Search from '../components/Search';
 import '../assets/styles/App.scss'; //Importando los estilos de este componente
 
 const App = () => (
     <div className="App">
         <Header></Header>
+        <Search></Search>
     </div>
 );
 
