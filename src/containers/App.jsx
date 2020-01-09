551502d1e21058daadd7c77146144e7406deeb62
Johan Mora <joenmodu99@gmail.com>
2020-01-09 16:16:58 -0400

    React Hooks: UseEffect y UseState

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'; //Use State para manejo de estado y effect para las trasmisiones hacer peticiones a un api o escuchar cambios de otras trasmiones entrantes
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -8,7 +8,20 @@ import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss'; //Importando los estilos de este componente
 
-const App = () => (
+const App = () => {
+
+    const [ videos, setVideos] = useState([]); //Inicializar el estados
+
+    useEffect(() => {//Recordar poner parametro para que escuche el useEffect sino quedara en un bucle infinito
+        fetch('http://localhost:3000/initalState')//Hace fetch a la api
+        .then(response => response.json())//La respuesta de la promesa la transforma en un json
+        .then(data => setVideos(data));//La data de la respuesta en json se la pasa a la funcion setVideos del estado
+    }, []);//parametro de useEffect
+
+    console.log('====================================');
+    console.log(videos);
+    console.log('====================================');
+    return (
     <div className="App">
         <Header></Header>
         <Search></Search>
@@ -44,5 +57,5 @@ const App = () => (
         </Categories>
     </div>
 );
-
+}
 export default App;
