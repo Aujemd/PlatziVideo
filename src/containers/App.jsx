c06b5ee6e8141cf5acc00c756bc043d7dc2811c0
Johan Mora <joenmodu99@gmail.com>
2020-01-10 13:17:52 -0400

    Custom Hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,43 +5,40 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState'; //Importando Hook
 
 import '../assets/styles/App.scss'; //Importando los estilos de este componente
 
+const API = 'http://localhost:3000/initalState';
 const App = () => {
-  const [videos, setVideos] = useState({
-    'mylist': [],
-    'trends': [],
-    'originals': [],
-  }); //Inicializar el estados
 
-  useEffect(() => { //Recordar poner parametro para que escuche el useEffect sino quedara en un bucle infinito
-    fetch('http://localhost:3000/initalState')//Hace fetch a la api
-      .then((response) => response.json())//La respuesta de la promesa la transforma en un json
-      .then((data) => setVideos(data));//La data de la respuesta en json se la pasa a la funcion setVideos del estado
-  }, []);//parametro de useEffect
+  const initialState = useInitialState(API);
 
-  console.log(videos);
 
-  return (
+  return initialState.length === 0 ? <h1>Loading ... </h1> :(
     <div className='App'>
       <Header />
       <Search />
-      <Categories title='Mi lista'>
-        <Carousel>
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {initialState.mylist.length > 0 &&
+       <Categories title='Mi lista'>
+       <Carousel>
+         <CarouselItem />
+       </Carousel>
+     </Categories>
+      }
+     
       <Categories title='Tendencias'>
         <Carousel>
           {
-            videos.trends.map((item) => <CarouselItem key = {item.id} {...item}></CarouselItem>)
+            initialState.trends.map((item) => <CarouselItem key = {item.id} {...item}></CarouselItem>)
           }
         </Carousel>
       </Categories>
       <Categories title='Originales de Platzi Videos'>
         <Carousel>
-          <CarouselItem />
+          {
+            initialState.originals.map((item) => <CarouselItem key = {item.id} {...item}/>)
+          }
         </Carousel>
       </Categories>
       <Footer />
