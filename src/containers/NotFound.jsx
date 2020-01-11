12b16f5905911237610ffc166fa8ebee8edb63a0
Johan Mora <joenmodu99@gmail.com>
2020-01-11 15:31:42 -0400

    Creación de container Not Found y uso de React Fragment

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//El fragment ahora un nodo en el arbol de nodos del dom para cuando se quiere enviar varios elementos
+const NotFound = () => (
+    <React.Fragment> 
+        <h1>No Encontrado</h1>
+    </React.Fragment>
+);
+
+export default NotFound;
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
 
 const App = () =>(
     <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () =>(
             <Route exact path ="/" component = {Home}></Route>
             <Route exact path ="/login" component = {Login}></Route>
             <Route exact path ="/register" component = {Register}></Route>
+            <Route component = {NotFound}></Route>
         </Switch>
     </BrowserRouter>
 )
