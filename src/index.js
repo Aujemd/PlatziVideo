05c49438c562a8fe69959a7e63a64b3b3f2525df
Johan Mora <joenmodu99@gmail.com>
2020-01-11 16:51:22 -0400

    Instalación de Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'//Importando react
 import ReactDOM from 'react-dom'//Importando react dom
+import {Provider} from 'react-redux'; //Encapsula los componentes y asi tener todo el store para todos los componentes
+import {createStore} from 'redux'; //Levanta el store
 import App from './routes/App'//Importando App routes
 
-ReactDOM.render(<App/>, document.getElementById("app"));//Rendereando el componente en el div app del public html
+ReactDOM.render(
+   
+       <Provider>
+            <App/>
+       </Provider>
+   , 
+    document.getElementById("app")
+);//Rendereando el componente en el div app del public html
