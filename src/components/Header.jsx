0678e8ae81d5e14b739f0acc3b65be6c15120f14
Johan Mora <joenmodu99@gmail.com>
2020-01-11 21:37:49 -0400

    Uso de gravatar en nuestro proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import '../assets/styles/components/Header.scss' //Importando los estilos de este componente
-import logo from '../assets/static/logo-platzi-video-BW2.png';
-import userIcon from '../assets/static/user-icon.png';
-import {Link} from  'react-router-dom';
+import React from "react";
+import "../assets/styles/components/Header.scss"; //Importando los estilos de este componente
+import logo from "../assets/static/logo-platzi-video-BW2.png";
+import userIcon from "../assets/static/user-icon.png";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import gravatar from "../utils/gravatar";
 
-const Header = () => (
+const Header = props => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  return (
     <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
 
-    <Link to = "/">
-      <img className="header__img" src={logo} alt="Platzi Video"/>
-    </Link>
-
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt=""/>
-        <p>Perfil</p>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (
+            <img src={userIcon} alt="" />
+          )}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <a href="/">Cuenta</a>
+          </li>
+          <li>
+            <Link to="/login">Iniciar Sesión</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><Link to="/login">Iniciar Sesión</Link></li>
-      </ul>
-    </div>
-  </header>
-);
-
-export default Header;
+    </header>
+  );
+};
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+export default connect(mapStateToProps, null)(Header);
