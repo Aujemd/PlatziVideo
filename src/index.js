3c19c4dc6e3f39cad6596fd509dde312f37c6260
Johan Mora <joenmodu99@gmail.com>
2020-01-12 21:03:23 -0400

    debug con redux y devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'//Importando react
 import ReactDOM from 'react-dom'//Importando react dom
 import {Provider} from 'react-redux'; //Encapsula los componentes y asi tener todo el store para todos los componentes
-import {createStore} from 'redux'; //Levanta el store
+import {createStore, compose} from 'redux'; //Levanta el store
 import App from './routes/App'//Importando App routes
 import reducer from './reducers'
 
@@ -171,7 +171,9 @@ const initialState = {
       ]
 }
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, initialState, composeEnhancers());
     ReactDOM.render(
        <Provider store={store}>
             <App/>
