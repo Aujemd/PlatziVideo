2219064fd26cf3d016b74ad3f7c8258604219954
Johan Mora <joenmodu99@gmail.com>
2020-02-08 18:56:23 -0400

    Aplicando  la funcion main para renderizado de servidor

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+require('ignore-styles') //Ignora estilos dentro del server
+
 require('@babel/register')({ //Paquete que nos permite bindear en realtime 
     ignore: [/(node_modules)/],
     presets: ['@babel/preset-env', '@babel/preset-react'],
---
+++ src/server/routes/main.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {renderToString} from 'react-dom/server' //renderizar componente a un string
+import { Provider } from 'react-redux'; //Encapsula los componentes y asi tener todo el store para todos los componentes
+import { createStore } from 'redux'; //Levanta el store
+import { StaticRouter } from 'react-router-dom'//Crea una ruta que no cambie para que el servidor no tena problema en crear rutas
+import { renderRoutes } from 'react-router-config'
+import Routes from '../../frontend/routes/serverRoutes'
+import Layout from '../../frontend/components/Layout'
+import reducer from '../../frontend/reducers/index'
+import initialState from '../../frontend/initialState'
+import render from '../render/index'
+
+const main = (req, res, next) => {
+    try{
+        const store = createStore(reducer, initialState)
+        const html = renderToString(
+            <Provider
+                store = {store}
+            >
+                <StaticRouter
+                    location = {req.url}
+                    context = {{}}
+                >
+                    <Layout>
+                        {renderRoutes(Routes)}
+                    </Layout>
+                </StaticRouter>
+            </Provider>
+        )
+        res.send(render(html))
+    }catch(err){
+        next(err)
+    }
+}
+
+export default main
+
